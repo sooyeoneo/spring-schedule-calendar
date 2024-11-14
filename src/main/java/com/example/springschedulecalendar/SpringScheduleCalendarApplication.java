0ba3ac90f4f51c496027cd2feb28979d711447c3
sooyeon_heo <suzi97@naver.com>
2024-11-15 08:58:36 +0900

    first commit

---
+++ src/main/java/com/example/springschedulecalendar/SpringScheduleCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschedulecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springschedulecalendar/SpringScheduleCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschedulecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
