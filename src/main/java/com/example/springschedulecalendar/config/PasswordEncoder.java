c89bf3981b4453ad5101b6df2291d1d12550cc82
sooyeon_heo <suzi97@naver.com>
2024-11-15 18:45:44 +0900

    fix : PasswordEncoder @Component 어노테이션 추가

--- src/main/java/com/example/springschedulecalendar/config/PasswordEncoder.java
+++ src/main/java/com/example/springschedulecalendar/config/PasswordEncoder.java
@@ -3,6 +3,7 @@ package com.example.springschedulecalendar.config;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
+@Component
 public class PasswordEncoder {
 
     public String encode(String rawPassword) {
