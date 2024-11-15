a74f454388a831557ecfc35d654db56975bf5a2a
sooyeon_heo <suzi97@naver.com>
2024-11-15 10:35:37 +0900

    feat : user, schedule entity 추가

---
+++ src/main/java/com/example/springschedulecalendar/entity/schedule/Schedule.java
@@ -0,0 +1,47 @@
+package com.example.springschedulecalendar.entity.schedule;
+
+import com.example.springschedulecalendar.entity.BaseEntity;
+import com.example.springschedulecalendar.entity.user.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    private Long id;
+
+    @Column()
+    private String title;
+
+//    @Setter
+    @Column(nullable = false) // columnDefinition = "longtext" 하면 Varchar보다 훨씬 긴 텍스트 저장 가능
+    private String contents;
+
+//    @Setter
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Schedule() {
+
+    }
+
+//    @OneToMany(mappedBy = "schedule", cascade = CascadeType.REMOVE, orphanRemoval = true)
+//
+//    public Schedule(String title, String contents) {
+//        this.title = title;
+//        this.contents = contents;
+//    }
+}
---
+++ src/main/java/com/example/springschedulecalendar/entity/user/User.java
@@ -0,0 +1,45 @@
+package com.example.springschedulecalendar.entity.user;
+
+import com.example.springschedulecalendar.entity.BaseEntity;
+import com.example.springschedulecalendar.entity.schedule.Schedule;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true) // nullable = false로 null 허용 안 함.
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Schedule> Schedules = new ArrayList<>();
+
+    public User() {
+
+    }
+
+//    public User(String userName, String email, String password) {
+//        this.userName = userName;
+//        this.email = email;
+//        this.password = password;
+//    }
+}
